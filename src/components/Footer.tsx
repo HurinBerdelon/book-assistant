25f4751388fbcd7f3c642b9f44573dee5f5454e0
Fernando Cardozo <fhpcardozo@gmail.com>
2024-09-23 21:44:49 -0300

    fix: fix mobile layout and add error handling for API calls

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+    <footer className="row-start-3 mt-8 flex gap-6 flex-wrap items-center justify-center">
       <span>Developed by:</span>
       <Link href="https://hurinberdelon.com">HurinBerdelon</Link>
     </footer>
